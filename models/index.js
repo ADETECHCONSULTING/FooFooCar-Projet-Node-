9f1bb7b1b91d316f4632aabea82d044c63c029f5
Adama Traoré <atraore@Rayonnance>
2018-03-23 18:18:14 +0100

    route get users fonctionne

--- models/index.js
+++ models/index.js
@@ -3,11 +3,12 @@ const Sequelize = require("sequelize");
 module.exports = server => {
 
     //setting up the connection config
-    let sequelize = new Sequelize("foofoocar", server.settings.username, server.settings.password, {
+    let sequelize = new Sequelize(server.settings.db_name, server.settings.username, server.settings.password, {
         dialect: server.settings.dialect
     });
 
 
+    //Models creations if not exists
     server.models = {
         Car: require('./Car')(sequelize),
         User: require('./User')(sequelize)
